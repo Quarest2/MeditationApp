b1ead969b979d40b8ae5b0bbe4fc2321b94e9602
Askar <Askar.Akhmetyanov@yandex.ru>
2025-03-13 17:57:03 +0300

    Initial Commit

---
+++ MeditationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeditationApp
+//
+//  Created by Аскар Ахметьянов on 13.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
